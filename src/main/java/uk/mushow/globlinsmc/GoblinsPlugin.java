8cedc79f1bc6bd8871a66392616659e5e90d62e3
Antoine James <antoine@mushow.uk>
2024-04-30 14:30:48 +0200

    Add basic plugin lifecycle logging

---
+++ src/main/java/uk/mushow/globlinsmc/GoblinsPlugin.java
@@ -0,0 +1,17 @@
+package uk.mushow.globlinsmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GoblinsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Goblins plugin enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Goblins plugin disabled");
+    }
+
+}
