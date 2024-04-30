a50132b32a25ad394d26597ba78747f4cc94949b
Antoine James <antoine@mushow.uk>
2024-04-30 17:18:05 +0200

    Add registerEvents() method

--- src/main/java/uk/mushow/goblinsmc/GoblinsPlugin.java
+++ src/main/java/uk/mushow/goblinsmc/GoblinsPlugin.java
@@ -1,17 +1,23 @@
-package uk.mushow.globlinsmc;
+package uk.mushow.goblinsmc;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import uk.mushow.goblinsmc.core.listeners.GoblinListener;
 
 public class GoblinsPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Goblins plugin enabled");
+        registerEvents();
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Goblins plugin disabled");
+    }
+
+    private void registerEvents() {
+        getServer().getPluginManager().registerEvents(new GoblinListener(), this);
     }
 
 }
