afa4fb0dab4b23320dadcafa3ca39762cc0fea6b
Antoine James <antoine@mushow.uk>
2024-04-30 21:19:58 +0200

    Add Enum Role.java;

---
+++ src/main/java/uk/mushow/goblinsmc/core/data/Role.java
@@ -0,0 +1,17 @@
+package uk.mushow.goblinsmc.core.data;
+
+public enum Role {
+
+    VILLAGER,
+    WEREWOLF,
+    SEER,
+    WITCH,
+    HUNTER,
+    CUPID,
+    THIEF,
+    GUARD,
+    LITTLE_GIRL,
+    RAT,
+    MAYOR
+
+}
