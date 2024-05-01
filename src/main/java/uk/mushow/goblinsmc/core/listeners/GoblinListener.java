e45de87775a3e8198dee65aa258c90c51d7b2b2c
Mushow <antoine@mushow.uk>
2024-05-01 05:14:53 +0200

    Add formatMessage to announce joining/leaving the game.

--- src/main/java/uk/mushow/goblinsmc/core/listeners/GoblinListener.java
+++ src/main/java/uk/mushow/goblinsmc/core/listeners/GoblinListener.java
@@ -1,5 +1,7 @@
 package uk.mushow.goblinsmc.core.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,9 +10,17 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class GoblinListener implements Listener {
 
     @EventHandler
-    private void onPlayerJoin(PlayerJoinEvent event) {}
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(formatMessage(event.getPlayer().getName(), Bukkit.getOnlinePlayers().size(), "joined"));
+    }
 
     @EventHandler
-    private void onPlayerQuit(PlayerQuitEvent event) {}
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(formatMessage(event.getPlayer().getName(), Bukkit.getOnlinePlayers().size() - 1, "left"));
+    }
+
+    private String formatMessage(String playerName, int onlineCount, String action) {
+        return ChatColor.GRAY + "(" + ChatColor.YELLOW + onlineCount + ChatColor.GRAY + "/" + ChatColor.YELLOW + Bukkit.getMaxPlayers() + ChatColor.GRAY + ") " + ChatColor.YELLOW + playerName + ChatColor.GRAY + " " + action + " the game.";
+    }
 
 }
