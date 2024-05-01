d5100ed7c2b6b92185686ae9a1a9568b93271b06
Mushow <antoine@mushow.uk>
2024-05-01 05:07:57 +0200

    Added Roles prefix and names

--- src/main/java/uk/mushow/goblinsmc/core/data/Role.java
+++ src/main/java/uk/mushow/goblinsmc/core/data/Role.java
@@ -1,17 +1,40 @@
 package uk.mushow.goblinsmc.core.data;
 
+import org.bukkit.ChatColor;
+
+import java.util.Random;
+
 public enum Role {
 
-    VILLAGER,
-    WEREWOLF,
-    SEER,
-    WITCH,
-    HUNTER,
-    CUPID,
-    THIEF,
-    GUARD,
-    LITTLE_GIRL,
-    RAT,
-    MAYOR
+    VILLAGER(ChatColor.GRAY, " Villager "),
+    WEREWOLF(ChatColor.RED, " Werewolf "),
+    SEER(ChatColor.BLUE, " Seer "),
+    WITCH(ChatColor.DARK_PURPLE, " Witch "),
+    HUNTER(ChatColor.DARK_GREEN, " Hunter "),
+    CUPID(ChatColor.LIGHT_PURPLE, " Cupid "),
+    THIEF(ChatColor.DARK_AQUA, " Thief "),
+    GUARD(ChatColor.DARK_GRAY, " Guard "),
+    LITTLE_GIRL(ChatColor.AQUA, " Little Girl "),
+    RAT(ChatColor.GOLD, " Rat ");
+
+    Role(ChatColor chatColor, String roleName) {
+        this.chatColor = chatColor;
+        this.roleName = roleName;
+    }
+
+    private final ChatColor chatColor;
+    private final String roleName;
+
+    private ChatColor getChatColor() {
+        return chatColor;
+    }
+
+    private String getRoleName() {
+        return roleName;
+    }
+
+    public String getRoleNameWithColor() {
+        return chatColor + roleName;
+    }
 
 }
